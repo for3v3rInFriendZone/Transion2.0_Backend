b1dc270422001ef0635735a923549b55c9e29527
for3v3rInFriendZone <strile93@gmail.com>
2018-04-17 00:17:57 +0200

    Added address repository.

--- src/main/java/com/example/Transion/app/repository/AddressRepository.java
+++ src/main/java/com/example/Transion/app/repository/AddressRepository.java
@@ -1,18 +1,13 @@
 package com.example.Transion.app.repository;
 
-import java.util.List;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.Transion.app.model.Address;
-import com.example.Transion.app.model.TransionUser;
 
 @Repository
 public interface AddressRepository extends MongoRepository<Address, ObjectId>{
 
-	@Query("{'address.country': ?0}")
-	public List<TransionUser> findByCountry(String country);
+	
 }
